48054647cbe72040f1ccab60ff0684f67394c1fd
Nynjin <moncef.hassani03@gmail.com>
2024-03-24 22:29:59 +0100

    feat: redirection to homepage

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/homepage',
+        permanent: true,
+      },
+    ];
+  }
 };
 
 export default nextConfig;
